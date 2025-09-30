ffafcaa7e1d0837435d8d2b9a04300ab93fce439
yaime07 <jaime.enrique.galvis@gmail.com>
2025-09-30 15:07:03 -0500

    to_summ

--- practica_1/to_sum.cpp
+++ practica_1/to_sum.cpp
@@ -24,36 +24,6 @@ int main() {
 
     cout << "Ingrese la lista de numeros separados por comas: ";
     string linea;
-    getline(cin, linea);
-
-    vector<int> nums;
-    stringstream ss(linea);
-    string token;#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <sstream>
-using namespace std;
-
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mapa; 
-        for (int i = 0; i < nums.size(); i++) {
-            int complemento = target - nums[i];
-            if (mapa.find(complemento) != mapa.end()) {
-                return {mapa[complemento], i};
-            }
-            mapa[nums[i]] = i;
-        }
-        return {}; 
-    }
-};
-
-int main() {
-    Solution sol;
-
-    cout << "Ingrese la lista de numeros separados por comas: ";
-    string linea;
     getline(cin, linea); 
 
     vector<int> nums;
@@ -66,7 +36,7 @@ int main() {
 
     cout << "Ingrese el objetivo (target): ";
     string targetStr;
-    getline(cin, targetStr);  
+    getline(cin, targetStr);
     int target = stoi(targetStr);
 
     vector<int> resultado = sol.twoSum(nums, target);
@@ -82,35 +52,6 @@ int main() {
         cout << "Salida: [" << resultado[0] << "," << resultado[1] << "]" << endl;
         cout << "Explicacion: nums[" << resultado[0] << "] + nums[" << resultado[1] 
              << "] = " << nums[resultado[0]] + nums[resultado[1]] << endl;
-    } else {
-        cout << "No se encontro solucion (aunque el problema garantiza que siempre hay una)." << endl;
-    }
-
-    return 0;
-}
-
-
-    while (getline(ss, token, ',')) {
-        nums.push_back(stoi(token));
-    }
-
-    int target;
-    cout << "Ingrese el objetivo (target): ";
-    cin >> target;
-
-    vector<int> resultado = sol.twoSum(nums, target);
-
-    if (!resultado.empty()) {
-        cout << "\nEntrada: nums = [";
-        for (int i = 0; i < nums.size(); i++) {
-            cout << nums[i];
-            if (i < nums.size() - 1) cout << ",";
-        }
-        cout << "], objetivo = " << target << endl;
-
-        cout << "Salida: [" << resultado[0] << "," << resultado[1] << "]" << endl;
-        cout << "Explicacion: nums[" << resultado[0] << "] + nums[" << resultado[1] << "] = " 
-             << nums[resultado[0]] + nums[resultado[1]] << endl;
     } else {
         cout << "No se encontro solucion (aunque el problema garantiza que siempre hay una)." << endl;
     }
